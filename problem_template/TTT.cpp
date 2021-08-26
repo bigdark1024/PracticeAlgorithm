705b324289eca3b346d4a2883664aa58a7196ad7
secr00t <secr00t@163.com>
2021-08-27 01:01:55 +0800

    feat:commit solution template.
    Commit leetcode solution template.
    
    issue:None

---
+++ problem_template/TTT.cpp
@@ -0,0 +1,15 @@
+#include "headers.h"
+
+class Solution {
+ public:
+
+};
+
+TEST(TTT, TTT) {
+    Solution s;
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
