b572833556c2c85d381d5ab5aaff00f7a56452a7
srcr00t <secr00t@163.com>
2021-08-29 20:45:51 +0800

    feat:create TreeNode struct.
    create TreeNode data struct in headers.h file.
    
    issue:None

--- include/headers.h
+++ include/headers.h
@@ -22,3 +22,12 @@
 #include <gtest/gtest.h>
 
 using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
