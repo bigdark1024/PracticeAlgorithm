300d1ae1919fe863228178e604c1343e0c16d34e
srcr00t <secr00t@163.com>
2021-08-29 20:50:39 +0800

    feat:add solution for leetcode102  .
    add solution for leetcode102 Binary Tree Level Order Traversal
    
    issue:None

---
+++ problems/T102_Binary_Tree_Level_Order_Traversal/T102_Binary_Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,21 @@
+#include "headers.h"
+
+class Solution {
+private:
+    vector<int> queue;
+    vector<vector<int>> ret; 
+ public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        return ret;
+    }
+};
+
+TEST(T102_Binary_Tree_Level_Order_Traversal, T102_Binary_Tree_Level_Order_Traversal) {
+    Solution s;
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
