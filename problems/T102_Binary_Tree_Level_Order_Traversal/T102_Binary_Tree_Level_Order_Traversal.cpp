89bb758c4ee095a4b9c0be6b0ff9ccfbb835e9e2
srcr00t <secr00t@163.com>
2021-08-29 23:37:17 +0800

    feat: add leetcode 102 solution
    102. Binary Tree Level Order Traversal
    
    issue:None

--- problems/T102_Binary_Tree_Level_Order_Traversal/T102_Binary_Tree_Level_Order_Traversal.cpp
+++ problems/T102_Binary_Tree_Level_Order_Traversal/T102_Binary_Tree_Level_Order_Traversal.cpp
@@ -1,12 +1,42 @@
 #include "headers.h"
 
-class Solution {
-private:
-    vector<int> queue;
+// 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+// 示例：
+// 二叉树：[3,9,20,null,null,15,7],
+// 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+class Solution 
+{
+ private:
     vector<vector<int>> ret; 
  public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        if (root == NULL) {
+            return ret;
+        }
+
+        queue<TreeNode*> que;
+        que.push(root);
+        while (que.size() > 0) {
+            int n = que.size();
+            vector<int> tmp;
+            while (n > 0) {
+                auto curr = que.front();
+                que.pop();
+                tmp.push_back(curr->val);
+
+                if (curr->left != NULL) {
+                    que.push(curr->left);
+                }
+
+                if (curr->right != NULL) {
+                    que.push(curr->right);
+                }
+                n--;
+            }
+            ret.push_back(tmp);
+        }
+
         return ret;
     }
 };
--- problems/solutions.md
+++ problems/solutions.md
@@ -1,6 +1,5 @@
 # leetcode solutions info
 ## sliding windows
-
 1208. Get Equal Substrings Within Budget
 209. Minimum Size Subarray Sum
 3. Longest Substring Without Repeating Characters
@@ -9,3 +8,6 @@
 
 ## string
 5. Longest Palindromic Substring
+
+## BFS
+102. Binary Tree Level Order Traversal
