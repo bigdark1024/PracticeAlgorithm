8f57cf01056c04808ff1329e0033ec03230665b6
secr00t <secr00t@163.com>
2021-10-01 22:10:35 +0800

    feat:update leetcode 1366 solution.
    1366. Rank Teams by Votes
    
    issue:None

---
+++ problems/T139_Word_break/T139_Word_Break.cpp
@@ -0,0 +1,16 @@
+#include "headers.h"
+
+class Solution {
+ public:
+
+};
+
+TEST(T139_Word_Break, testcase1) {
+  Solution s;
+
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
