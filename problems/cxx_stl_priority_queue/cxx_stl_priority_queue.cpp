9bb804e41956ff05e5fdb29fef08b30e463ca924
secr00t <secr00t@163.com>
2021-10-07 16:06:57 +0800

    feat:add stl priority_que.
    stl priority_que demo
    
    issue:None

--- problems/cxx_stl_priority_queue/cxx_stl_priority_queue.cpp
+++ problems/cxx_stl_priority_queue/cxx_stl_priority_queue.cpp
@@ -1,24 +1,53 @@
 #include "headers.h"
 
-TEST(cxx_stl_priority_queue, cxx_stl_priority_queue) {
-    priority_queue<int> que;
-    que.push(7);
-    que.push(5);
-    que.push(-2);
-    que.push(1);
-    que.push(6);
+template<typename T>
+void print_queue(T &q) {
+  while (!q.empty()) {
+    std::cout << q.top() << " ";
+    q.pop();
+  }
+  std::cout << '\n';
+}
 
-    cout << que.size() << endl;
-    while(!que.empty()) {
-        cout << que.top() << " ";
-        que.pop();
-    }
+TEST(cxx_stl_priority_queue, testcase1) {
+  priority_queue<int> que; // default greater priority queue
+  que.push(7);
+  que.push(5);
+  que.push(-2);
+  que.push(1);
+  que.push(6);
 
-    cout << endl;
-    return 0;
-} 
+  cout << que.size() << endl;
+  print_queue(que);
+
+  priority_queue<int, vector<int>, std::less<int>> que2;  // less<int> 大顶堆
+  for (int n: {1, 8, 5, 6, 3, 4, 0, 9, 7, 2}) {
+    que2.push(n);
+  }
+  print_queue(que2);
+}
+
+struct node {
+  int x, y;
+  bool operator<(const node &n) const {
+    return this->x < n.x; // order by x in decending order
+  }
+};
+
+TEST(cxx_stl_priority_queue, testcase2) {
+  priority_queue<node> que;
+  que.push((node) {1, 5});
+  que.push((node) {2, 3});
+  que.push((node) {-5, 7});
+
+  while (!que.empty()) {
+    cout << que.top().x << " ";
+    que.pop();
+  }
+  cout << endl;
+}
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
