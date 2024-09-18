e2fafc1e9475516da27a8f6a642c853c01cf9552
Anthony's Pub <secr00t@163.com>
2024-09-19 05:02:53 +0800

    update sprintf_s

--- problems/jiqiao_num_str_convert/num_str_convert.cpp
+++ problems/jiqiao_num_str_convert/num_str_convert.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <gtest/gtest.h>
-#include <stdlib.h>
+#include <cstdio>
 
 using namespace std;
 
@@ -15,6 +15,22 @@ TEST(jiqiao_num_str_convert, t1)
     itoa(num2, str,2);
     printf("num : %d comnvert to string: %s \n", num2, str);
 }
+
+/* 整数转字符串 */
+TEST(jiqiao_num_str_convert, t2)
+{
+    int num1 = 123456;
+    char buffer[512] = {0};
+    size_t buffer_size = sizeof(buffer);
+    int str_len = _snprintf_s(buffer, buffer_size, buffer_size - 1,
+                              "%5o", num1);
+    if (str_len < 0) {
+        return;
+    }
+    buffer_size -= str_len;
+    printf("num : %d comnvert to string: %s \n", num1, buffer);
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
